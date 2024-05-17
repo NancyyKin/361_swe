2a199179f4360d84cdc6af23b05e8baef494ba0f
Nancy Yang <yangnan@oregonstate.edu>
2024-05-16 22:08:21 -0700

    updated file order

--- script.js
+++ script.js
@@ -277,3 +277,24 @@ document.querySelector('#yesColor').addEventListener("click", () => {
     addColorToHistoryBar(color, currentPosition);
     showIndColorPage(color);
 })
+
+
+const amqp = require("amqplib");
+
+const msg = { color: selectedColor }
+connect();
+async function connect() {
+
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672")
+        const channel = await connection.createChannel();
+        const result = await channel.assertQueue("jobs");
+        channel.sendToQueue("jobs", Buffer.from(JSON.stringify(msg)));
+        console.log(`Job sent sucessfully --> ${msg.note}`);
+        await channel.close();
+        await connection.close();
+    }
+    catch (ex) {
+        console.error(ex)
+    }
+}
